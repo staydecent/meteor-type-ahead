4dd0d16b77a6ab8a8650e644453501faa56ce920
Adrian Unger <dev@staydecent.ca>
2016-04-14 13:36:45 -0700

    Setup default values and handle attributes.

--- imports/ui/typeAhead.js
+++ imports/ui/typeAhead.js
@@ -1,6 +1,23 @@
 import { Template } from 'meteor/templating';
+import { ReactiveDict } from 'meteor/reactive-dict';
+
 import './typeAhead.html';
 
+const INITIAL_POS = 0;
+
+Template.typeAhead.onCreated(function() {
+  console.log('typeAhead', this.data);
+
+  this.state = new ReactiveDict();
+  this.state.setDefault({
+    currPos: this.data.currPos || INITIAL_POS,
+    suggestions: this.data.suggestions || [],
+    showSuggestions: this.data.showSuggestions || false,
+    threshold: this.data.threshold || 2,
+    limit: this.data.limit || Infinity,
+    delay: this.data.delay || 100,
+  });
+});
  
 Template.typeAhead.events({
   'input .search'(event) {
